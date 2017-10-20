1ac6837406921d3ae6debc358be984cd399051d3
Dinh Lam <dinhlam@Dinh-Lam.local>
2017-10-20 16:32:23 +0700

    update animation

--- js/service.js
+++ js/service.js
@@ -1,5 +1,5 @@
 $(function(){
-    var date = new Date(2017, 9, 21);
+    var date = new Date(2017, 9, 30);
     var now = new Date();
     var diff = (date.getTime()/1000) - (now.getTime()/1000);
 
@@ -7,25 +7,49 @@ $(function(){
         clockFace: 'DailyCounter',
         countdown: true
     });
-        var a = -80;
+        var a = -200;
         var num = 2;
         var oldScrollTop = 0;
-        var scrollTop
+        var scrollTop;
+        var scrollSpeed = 5;
     $(window).scroll(function() {
         oldScrollTop = scrollTop;
         scrollTop = $(this).scrollTop();
-        if(scrollTop < oldScrollTop){}
-        else if(scrollTop > $('#subscribe-bottom-section').offset().top){
+        // set parallax
+        var topParallax = $('#subscribe-bottom-section').offset().top;
+        var heightOfParentParallax = $('#subscribe-bottom-section')[0].clientHeight;
+        $('#subscribe-bottom-section').css('height', heightOfParentParallax);
+        $('#subscribe-bottom-section .parallax').css('height', heightOfParentParallax + 200);
+        if(scrollTop - topParallax > heightOfParentParallax){
+            console.log('scroll du vay')
             a = 0;
-            $("#subscribe-bottom-section").css("background-position","center"+" "+a+"px");
-        }else if(scrollTop < $('#subscribe-bottom-section').offset().top - 500 ){
-            a = -80;
-             $("#subscribe-bottom-section").css("background-position","center"+" "+a+"px");
-        }else{
-             if(a < 0){
-                 a = a + (($('#subscribe-bottom-section').offset().top - scrollTop) / 5);
-             }
-             $("#subscribe-bottom-section").css("background-position","center"+" "+a+"px");
+            $('#subscribe-bottom-section .parallax').css('background-position', 'center '+ a+ 'px');
+        }
+        if (topParallax  - scrollTop < 200){
+            a += 0;
+            console.log('topParallax  - scrollTop < 200')
+            $('#subscribe-bottom-section .parallax').css('background-position', 'center '+ a+ 'px');
+        }
+        if(topParallax - scrollTop > 600){
+            a = -200;
+            console.log('topParallax - scrollTop > 600')
+            $('#subscribe-bottom-section .parallax').css('background-position', 'center '+ a+ 'px');
+        }
+        if(topParallax - scrollTop > 200 && topParallax - scrollTop < 600){
+            console.log('vao 200 600')
+            //if direction down
+            if(scrollTop > oldScrollTop){
+                console.log('down')
+                if(a != 0 && a + 200/100 < 0){
+                    a += 200/150;
+                }else{ a = 0;}
+            }else
+            {
+                // direction up
+                console.log('up')
+                a -= 200/1000;
+            }
+            $('#subscribe-bottom-section .parallax').css('background-position', 'center '+ a+ 'px');
         }
 
         $('#youtube-section').css({
@@ -42,4 +66,30 @@ $(function(){
             }
         });
     });
+
+    //Handle animation
+    function onScrollInit( items, trigger ) {
+        items.each( function() {
+            var osElement = $(this),
+                osAnimationClass = osElement.attr('data-os-animation'),
+                osAnimationDelay = osElement.attr('data-os-animation-delay');
+            
+                osElement.css({
+                '-webkit-animation-delay':  osAnimationDelay,
+                '-moz-animation-delay':     osAnimationDelay,
+                'animation-delay':          osAnimationDelay
+                });
+
+                var osTrigger = ( trigger ) ? trigger : osElement;
+                
+                osTrigger.waypoint(function() {
+                osElement.addClass('animated').addClass(osAnimationClass);
+                },{
+                    triggerOnce: true,
+                    offset: '75%'
+                });
+        });
+    }
+
+    onScrollInit( $('.os-animation') );
 })
